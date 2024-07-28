8317d3a6d8937b418eef11a7e18ccfb0c148d39a
Mkhalid000 <khalid@affyclouditsolutions.com>
2024-07-28 23:50:21 +0530

    Create Setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css"
 import { Home } from "./Pages/Home";
-import { About } from "./Pages/About";
-import { Contact } from "./Pages/Contact";
-import { Digital } from "./Pages/Digital";
-import { NotFound } from "./Pages/NotFound";
-import { HXM } from "./Pages/HXM";
-import { HANA } from "./Pages/HANA";
-import { Analytics } from "./Pages/Analytics";
-import { Business } from "./Pages/Business";
-import { Customer } from "./Pages/Customer";
-import { Cloud } from "./Pages/Cloud";
-import { Sales } from "./Pages/Sales";
-import { Salesforce } from "./Pages/Salesforce";
 
 function App() {
   return (
@@ -20,18 +8,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/sap_hxm" element={<HXM />} />
-          <Route path="/hana" element={<HANA />} />
-          <Route path="/analytics" element={<Analytics />} />
-          <Route path="/business" element={<Business />} />
-          <Route path="/customer" element={<Customer />} />
-          <Route path="/digital" element={<Digital />} />
-          <Route path="/cloud" element={<Cloud />} />
-          <Route path="/Sales_cloud" element={<Sales />} />
-          <Route path="/sales_force" element={<Salesforce />} />
-          <Route path="*" element={<NotFound />} />
+          
         </Routes>
       </Router>
     </div>
--- src/Componants/utility.js
+++
@@ -1,5 +0,0 @@
-export const enforceMaxLength = (event) => {
-    if (event.target.value.length > 10) {
-      event.target.value = event.target.value.slice(0, 10);
-    }
-  };
